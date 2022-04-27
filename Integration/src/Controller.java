058dce646d6a244a3a565594fc354a29ebe6e484
Kirazin0 <troopex@hotmail.com>
2022-04-27 12:45:36 +0200

    LogManager added

--- Integration/src/Controller.java
+++ Integration/src/Controller.java
@@ -1,11 +1,13 @@
 import Model.FileManager;
-
+import Model.LogManager; 
 public class Controller {
 
 	public static void main(String[] args) {
 		FileManager file = new FileManager();
 		file.getXmlFile();
-
+		//LogManager logManager = new LogManager("logFile.log"); 		
+		//logManager.logInfo("bla bla bla");
+		//logManager.logError("this is an error!"); 
 	}
 
 }
