4b646b5eb3c961b7fc3c7ca1f9826355a32ade1b
MinhThai098 <thaimkay@hotmail.com>
2022-05-20 10:11:35 +0200

    implemented clock function

--- Integration/src/Controller.java
+++ Integration/src/Controller.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+import Model.Clock;
 import Model.DollibarConnect;
 import Model.Email;
 import Model.FileManager;
@@ -20,6 +21,8 @@ public static void main(String[] args) {
 		DollibarConnect dolibarrConnect = new DollibarConnect(logManager); 
 		Validator validator = new Validator(logManager); 
 		Email email = new Email(logManager); 
+		
+
 	
 		// Configure settings
 		String path = "config.properties";
@@ -32,61 +35,65 @@ public static void main(String[] args) {
 
 		// Gets leads from URL		
 		// Gets new leads from URL		
-		Boolean isSuccessful = fileManager.getUrlResponse();
 
+		Clock clock = new Clock();
+		Boolean isSuccessful = false;
 		
-		if(isSuccessful) {
-	
-		
-			// Gets the new leads from XML file
-			ArrayList<Lead> leadList = fileManager.getLeadsFromXML();
-
-			System.out.println(leadList.size());
-			System.out.println(lastWeekLeadList.size());
-
+		// while loop to ensure it tries to get XML document from HTTP URL more than once.
+		while (!isSuccessful){
+			clock.checkTime();
+			isSuccessful = fileManager.getUrlResponse();
 			
-			// Compares new list to last weeks list
-			 leadList = validator.compareLeadLists(lastWeekLeadList, leadList); 
-
-			 // If theres no changes to leadlist
-			 if(leadList.size() == 0) {
-				 // send email
-				 	email.sendEmail("Integration Lion error", "No new leads from webscrapers this week. Check the latest log file for more info");
-				 
-			 }else {
-					// Verifies leadList and saves it to verrifiedList
-					
-				 // Verifies leadList
-					ArrayList<Lead> verrifiedList = validator.validateLeads(leadList); 
-				
-					
-					if(verrifiedList.size() != 0) {
-							// Imports the new verrified leads to dolibarr
-							dolibarrConnect.importLeads(verrifiedList);
-
-				// 	If new verrified list size is == 0		
-				} else {
-					email.sendEmail("", "");
-
-				}
-
-			 }
-
-				dolibarrConnect.removeLead();
-
-			 dolibarrConnect.removeLead();
-
-		// If response from URL does not work
-		}else {
-			
-			 //send e-mail
+			// If response from URL does not work
+			if (!isSuccessful) {
+				//send e-mail
 				email.sendEmail("Integration Lion error", "Could not download files from webscrapers' URL. Check the latest log file for more info");
+			}
+			
 		}
+
 		
 
+		
+		// Gets the new leads from XML file
+		ArrayList<Lead> leadList = fileManager.getLeadsFromXML();
+
+		System.out.println(leadList.size());
+		System.out.println(lastWeekLeadList.size());
+
+		
+		// Compares new list to last weeks list
+		 leadList = validator.compareLeadLists(lastWeekLeadList, leadList); 
+
+		 // If theres no changes to leadlist
+		 if(leadList.size() == 0) {
+			 // send email
+			 	email.sendEmail("Integration Lion error", "No new leads from webscrapers this week. Check the latest log file for more info");
+			 
+		 }else {
+				// Verifies leadList and saves it to verrifiedList
 				
+			 // Verifies leadList
+				ArrayList<Lead> verrifiedList = validator.validateLeads(leadList); 
+			
+				
+				if(verrifiedList.size() != 0) {
+						// Imports the new verrified leads to dolibarr
+						dolibarrConnect.importLeads(verrifiedList);
+
+			// 	If new verrified list size is == 0		
+			} else {
+				email.sendEmail("", "");
+
+			}
+
+		 }
+
+			dolibarrConnect.removeLead();
+
+		 dolibarrConnect.removeLead();
+
+
 		
-		
-	
 	}
 	
