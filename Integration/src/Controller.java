b8c93b0dedf5e09544463bda83e8564756155499
Kirazin0 <troopex@hotmail.com>
2022-05-20 10:58:10 +0200

    **** bugfix
    USHCH

--- Integration/src/Controller.java
+++ Integration/src/Controller.java
@@ -41,20 +41,15 @@ public static void main(String[] args) {
 			// Gets the new leads from XML file
 			ArrayList<Lead> leadList = fileManager.getLeadsFromXML();
 
-			System.out.println(leadList.size());
-			System.out.println(lastWeekLeadList.size());
-
 			
 			// Compares new list to last weeks list
 			 leadList = validator.compareLeadLists(lastWeekLeadList, leadList); 
-
 			 // If theres no changes to leadlist
 			 if(leadList.size() == 0) {
 				 // send email
-				 	email.sendEmail("Integration Lion error", "No new leads from webscrapers this week. Check the latest log file for more info");
+				 //	email.sendEmail("Integration Lion error", "No new leads from webscrapers this week. Check the latest log file for more info");
 				 
 			 }else {
-					// Verifies leadList and saves it to verrifiedList
 					
 				 // Verifies leadList
 					ArrayList<Lead> verrifiedList = validator.validateLeads(leadList); 
@@ -62,11 +57,12 @@ public static void main(String[] args) {
 					
 					if(verrifiedList.size() != 0) {
 							// Imports the new verrified leads to dolibarr
+						
 							dolibarrConnect.importLeads(verrifiedList);
 
 				// 	If new verrified list size is == 0		
 				} else {
-					email.sendEmail("", "");
+				//	email.sendEmail("", "");
 
 				}
 
@@ -74,19 +70,17 @@ public static void main(String[] args) {
 
 				dolibarrConnect.removeLead();
 
-			 dolibarrConnect.removeLead();
-
 		// If response from URL does not work
 		}else {
 			
 			 //send e-mail
-				email.sendEmail("Integration Lion error", "Could not download files from webscrapers' URL. Check the latest log file for more info");
+			//	email.sendEmail("Integration Lion error", "Could not download files from webscrapers' URL. Check the latest log file for more info");
 		}
 		
 
 				
 		
-		
+}	
 	
 	}
 	
